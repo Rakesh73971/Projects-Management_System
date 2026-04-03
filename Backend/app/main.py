cc6bad889268140686c444ce3db3a8d5a3747b39
Rakesh N <nraki522@gmail.com>
2026-04-04 01:19:58 +0530

    added the middelware

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -3,11 +3,23 @@ from . import models
 from .database import engine
 from .routers import oauth,user,oraganization_member,organization,project,task
 from .ai import ai_router
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
 
 #models.Base.metadata.create_all(bind=engine)
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,             
+    allow_credentials=True,
+    allow_methods=["*"],       
+    allow_headers=["*"],               
+)
 
 app.include_router(oauth.router)
 app.include_router(user.router)
