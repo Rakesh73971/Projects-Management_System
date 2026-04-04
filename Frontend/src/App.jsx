363bebc9159c13af2c72cce5fdaa56d72c07ab67
Rakesh N <nraki522@gmail.com>
2026-04-04 20:50:07 +0530

    Added the routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,29 +1,18 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// src/App.jsx
+import { BrowserRouter } from "react-router-dom";
 
 import { AuthProvider } from "./context/AuthContext";
 import { OrgProvider } from "./context/OrgContext";
+import AppRoutes from "./routes/AppRoutes.jsx";
 
-import Layout from "./components/layout/Layout";
-import Login from "./pages/Login"; 
-import Register from "./pages/Register";
-
-function App() {
+export default function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
         <OrgProvider>
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-
-            <Route path="/" element={<Layout />} />
-            <Route path="/dashboard" element={<Layout />} />
-          </Routes>
+          <AppRoutes />
         </OrgProvider>
       </AuthProvider>
     </BrowserRouter>
   );
 }
-
-export default App;
